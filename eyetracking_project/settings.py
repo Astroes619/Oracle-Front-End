10cc1c5efee9db77a1424cadcec1e5ffd2e2eb64
Atheeque Hasan <aiaahasan@students.nsbm.ac.lk>
2023-04-08 00:43:03 +0530

    settings

--- eyetracking_project/settings.py
+++ eyetracking_project/settings.py
@@ -135,8 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
     # Configure static files settings for production
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'eyetracking_app/static')]
 
